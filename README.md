5015f863c5bb00ededbe951b8c47ec46c820781c
Elvira_Sinyanskaya <elvira.sinyanskaya@gmail.com>
2021-12-10 22:50:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Countdown timer till New Years Eve
+
+It is my first project with JavaScript. I wanted to make a site, that counts how many days left till New Year.
+
+
+### Screenshot
+
+![](./screenshot.png)
+
+
+### Links
+
+- Live Site URL: [](https://bitterrsweetj.github.io/countdown-timer/)
+
+
+## Author
+
+- Instagram - [@bitterrsweetj](https://www.instagram.com/bitterrsweetj)
+
+
---
+++ app.js
@@ -0,0 +1,26 @@
+const countdown = () => {
+    const countDate = new Date('Jan 1, 2022 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const daysLeft = Math.floor(gap / day);
+    const hoursLeft = Math.floor((gap % day) / hour);
+    const minutesLeft = Math.floor((gap % hour) / minute);
+    const secondsLeft = Math.floor((gap % minute) / second);
+
+    document.querySelector("#days").innerText = formatTime(daysLeft);
+    document.querySelector("#hours").innerText = formatTime(hoursLeft);
+    document.querySelector("#mins").innerText = formatTime(minutesLeft);
+    document.querySelector("#secs").innerText = formatTime(secondsLeft);
+
+    function formatTime(time){
+        return time < 10 ? (`0${time}`) : time;
+    }
+};
+
+setInterval(countdown, 1000);
